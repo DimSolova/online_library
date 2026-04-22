35bc43cb88071f850a5b7f06547910185211bb8b
Solova_Mac <dimsolova@gmail.com>
2026-04-22 12:22:31 +0300

    add unit and integration tests

---
+++ test/unit_test/test_auth.py
@@ -0,0 +1,7 @@
+from src.services.users import UserService
+
+def test_create_access_token():
+    data = {"user_id": 1}
+    jwt_token = UserService().create_access_token(data)
+    assert jwt_token
+    assert isinstance(jwt_token, str)
