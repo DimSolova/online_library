642b0b798ee61abebb65fd8b1b14d5dc321a3f64
Solova_Mac <dimsolova@gmail.com>
2026-04-28 12:19:29 +0300

    pytest add PATCH auth/id/active

--- test/integration_tests/users/test_admin_api.py
+++ test/integration_tests/users/test_admin_api.py
@@ -12,8 +12,11 @@ from test.helpers.auth import login_as_author
     ("admin", 55, 3, 401),
     # не существует роли
     ("admin", 55, 5, 401),
+
+    ("admin", 3, 2, 200),
+    ("admin", 2, 2, 200),
 ])
-async def test_change_user_role(user, user_id, role_id, status_code,ac):
+async def test_change_user_role(user, user_id, role_id, status_code, ac, db):
     resp_admin = await login_as_author(ac, user)
     response = await resp_admin.patch(f"/auth/{user_id}/role",
                            json={
@@ -21,3 +24,35 @@ async def test_change_user_role(user, user_id, role_id, status_code,ac):
                            }
                            )
     assert response.status_code == status_code
+    if status_code != 200:
+        return
+    user = await db.users.get_one(id=user_id)
+    assert user.role_id == role_id
+
+@pytest.mark.parametrize("user, user_id, is_active, status_code",[
+    ("admin", 2, True, 200),
+    # Проверка на автора
+    ("author1", 2, False, 403),
+    # проверка на Пользователя
+    ("testuser", 2, False, 403),
+    # не существует пользователя
+    ("admin", 55, True, 401),
+    #неверный ввод
+    ("admin", 2, 'qwe', 422),
+    #изменяем роль
+    ("admin", 2, False, 200),
+    #Изменяем обратно
+    ("admin", 2, True, 200),
+])
+async def test_change_user_active(user, user_id, is_active, status_code,ac, db):
+    resp_admin = await login_as_author(ac, user)
+    response = await resp_admin.patch(f"/auth/{user_id}/active",
+                           json={
+                               "is_active": is_active
+                           }
+                           )
+    assert response.status_code == status_code
+    if status_code != 200:
+        return
+    user = await db.users.get_one(id=user_id)
+    assert user.is_active == is_active
