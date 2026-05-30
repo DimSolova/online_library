8370e04222383042505d602806cb81dff8b22b1b
Solova_Mac <dimsolova@gmail.com>
2026-05-30 13:40:58 +0300

    add notifications with review говнокод

--- src/schemas/notifications.py
+++ src/schemas/notifications.py
@@ -12,3 +12,10 @@ class NotificationDTO(BaseModel):
     related_review_id: int
     is_read: bool
     created_at: datetime
+
+class NotificationAddDTO(BaseModel):
+    user_id: int
+    title: str
+    message: str
+    related_book_id: int
+    related_review_id: int
--- src/services/reviews.py
+++ src/services/reviews.py
@@ -4,6 +4,7 @@ from src.exceptions import (
     ObjectAlreadyExistsException,
     ReviewAlreadyExistsException,
 )
+from src.schemas.notifications import NotificationAddDTO
 from src.schemas.reviews import ReviewAddDTO, ReviewAddRequestDTO
 from src.services.base import BaseService
 
@@ -13,7 +14,27 @@ class ReviewsService(BaseService):
         add_review = ReviewAddDTO(**data.model_dump(), user_id=user.id, book_id=book_id)
         try:
             res = await self.db.review.add(add_review)
+
+            #### Пишу откровенный говонокод на добавлени
+            book = await self.db.books.get_one(id=book_id)
+            #### Пишу откровенный говонокод на добавлени
+            user_id = book.added_by_id
+            text = f"вашу книгу оценили в {data.rating}"
+            message = data.text
+            rel_book_id = book_id
+
+
+            notifications = NotificationAddDTO(
+                user_id=user_id,
+                title=text,
+                message=message,
+                related_book_id=rel_book_id,
+                related_review_id=res.id
+            )
+            await self.db.notifications.add(notifications)
+
             await self.db.commit()
+
         except ObjectAlreadyExistsException:
             raise ReviewAlreadyExistsException
         except ForeignKeyException:
